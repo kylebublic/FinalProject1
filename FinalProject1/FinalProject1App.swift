6016d3c19d656d416ce105ad0e54b45c04bdd43b
kylebublic <kylegale@unc.edu>
2026-04-08 19:04:38 -0400

    Initial Commit

---
+++ FinalProject1/FinalProject1App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject1App.swift
+//  FinalProject1
+//
+//  Created by Kyle Bublic on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
